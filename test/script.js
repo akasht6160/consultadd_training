687f1085066305296180b6a70ec35cbe6099f882
akasht6160 <akash.t6160@gmail.com>
2022-03-28 01:53:09 -0700

    initial commit

---
+++ test/script.js
@@ -0,0 +1,5 @@
+
+
+// simple logging
+
+console.log("Hello World")
---
+++ windows_python_setup.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+pip install Django
+pip install Flask
+
+pip install matplotlib
+pip install numpy
+pip install opencv-python
+pip install virtualenv
+pip install pandas
+
+pip install django-crispy-forms
+pip install crispy-bootstrap5
+
